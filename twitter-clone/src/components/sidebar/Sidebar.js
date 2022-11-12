edfe9cf9167d0e58a844719b723b7708c7771f73
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-12 16:23:42 +0900

    chore: マテリアルUI導入

---
+++ twitter-clone/src/components/sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <TwitterIcon className="sidebar__twitter-icon" />
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* ツイートボタン */}
+    </div>
+  );
+};
+
+export default Sidebar;
