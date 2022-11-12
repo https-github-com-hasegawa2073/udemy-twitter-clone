5c5b536f86f0b31504b81e0bba1700dcf6da47af
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-12 20:51:34 +0900

    chore: firebaseの初期化

---
+++ twitter-clone/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1D48rVemXv24nUX_QjjUIoc3oCg6ZAQw",
+  authDomain: "twitter-clone-udemy-b0f46.firebaseapp.com",
+  projectId: "twitter-clone-udemy-b0f46",
+  storageBucket: "twitter-clone-udemy-b0f46.appspot.com",
+  messagingSenderId: "934623556821",
+  appId: "1:934623556821:web:930f7bf8c0525932165e5c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
